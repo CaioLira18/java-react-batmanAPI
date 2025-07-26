23cb2e7e24717faa318586c66261c3720156b7ac
CaioLira18 <caiozao1212@gmail.com>
2025-07-26 15:13:00 -0300

    [feat] - update

---
+++ front/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <section className='characters'>
+        <div className="characterLine">
+            <div className='characterContainer'>
+              <div className="characterBoxContainer">
+                <div className="characterBox"></div>
+                <div className="characterBox"></div>
+                <div className="characterBox"></div>
+                <div className="characterBox"></div>
+                <div className="characterBox"></div>
+                <div className="characterBox"></div>
+                <div className="characterBox"></div>
+                <div className="characterBox"></div>
+                <div className="characterBox"></div>
+                <div className="characterBox"></div>
+                <div className="characterBox"></div>
+                <div className="characterBox"></div>
+                <div className="characterBox"></div>
+              </div>
+            </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default App
